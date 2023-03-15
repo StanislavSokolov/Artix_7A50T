895bfa8433597caa35841196d6e9f8af2a130d3c
sokolovstanislav <ignateva-1946@mail.ru>
2023-03-15 16:52:24 +0300

    Prepared to pullrequest
    Prepared to pullrequest

--- Artix_7A50T.cache/ip/2019.1/9ba2aa5477d478ed/design_1_axi_intc_0_0_sim_netlist.v
+++ Artix_7A50T.cache/ip/2019.1/9ba2aa5477d478ed/design_1_axi_intc_0_0_sim_netlist.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-// Date        : Mon Mar 13 18:00:49 2023
+// Date        : Tue Mar 14 09:01:33 2023
 // Host        : STAS-W10 running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode funcsim -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
 //               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ design_1_axi_intc_0_0_sim_netlist.v
